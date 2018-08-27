be48fddc2b20016df34821a422a378b7037364b1
FletcherFT <fletcher.thompson@utas.edu.au>
2018-08-27 17:10:04 +1000

    python2 fix to serialhandler.py

--- digi/xbee/reader.py
+++ digi/xbee/reader.py
@@ -12,7 +12,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-from queue import Queue, Empty
+from Queue import Queue, Empty
 import logging
 import threading
 import time
